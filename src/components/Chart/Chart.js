3e3aefec3166327f29cf7b488cc546b49174b3bf
aisiklar <maisiklar@gmail.com>
2022-04-29 17:21:44 +0300

    adding the chart but not finished yet

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ChartBar from './ChartBar';
+import './Chart.css';
+
+const Chart = (props) => {
+
+    return (
+        <div className="class">
+            {props.dataPoints.map(dataPoint =>{
+                <ChartBar 
+                    key={dataPoint.label}
+                    value={dataPoint.value} 
+                    maxValue={null} 
+                    label={dataPoint.label}>
+                </ChartBar>
+            })}
+        </div>
+    )
+}
+
+export default Chart;
+
---
+++ src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Chart from '../Chart/Chart';
+
+
+const ExpenseChart = (props) => {
+
+    return ()
+}
+
+export default ExpenseChart;
